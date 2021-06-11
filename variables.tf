193b2da5c13a891ef94abcb2e59bf8e9df6ae108
M shanmukha nath reddy <55055853+LionHuntDiG@users.noreply.github.com>
2021-06-11 10:25:16 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -54,3 +54,16 @@ variable "network_service_cidr" {
   description = "CNI service cidr"
   default     = "10.2.0.0/24"
 }
+variable log_analytics_workspace_name {
+    default = "testLogAnalyticsWorkspaceName"
+}
+
+# refer https://azure.microsoft.com/global-infrastructure/services/?products=monitor for log analytics available regions
+variable log_analytics_workspace_location {
+    default = "eastus"
+}
+
+# refer https://azure.microsoft.com/pricing/details/monitor/ for log analytics pricing 
+variable log_analytics_workspace_sku {
+    default = "PerGB2018"
+}
