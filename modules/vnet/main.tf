f7123e3a496c5f9df7e19a095ad9185549339075
shanmukha reddy <nath.shanmukha@gmail.com>
2021-06-28 10:51:52 +0000

    Updated main.tf

--- modules/vnet/main.tf
+++ modules/vnet/main.tf
@@ -6,17 +6,11 @@ resource "azurerm_virtual_network" "vnet" {
 }
 
 resource "azurerm_subnet" "subnet" {
-  
-  name                 = "subnet-aks"
+  for_each = { for subnet in var.subnets : subnet.name => subnet.address_prefixes }
+
+  name                 = each.key
   resource_group_name  = var.resource_group_name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     =  ["10.0.1.0/24"]
-  delegation {
-    name = "delegation"
-
-    service_delegation {
-      name    = "Microsoft.ContainerInstance/containerGroups"
-      actions = ["Microsoft.Network/virtualNetworks/subnets/join/action", "Microsoft.Network/virtualNetworks/subnets/prepareNetworkPolicies/action"]
-    }
-  }
+  address_prefixes     = each.value
+  enforce_private_link_service_netwrok_policies = false
 }
